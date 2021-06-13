1d3a97032cd589d75e791dc0b236ae5c027e6a27
Animesh Sharma <animesharma3@gmail.com>
2021-06-13 12:55:36 +0530

    CORS Header Whitelisting localhost

--- confessions/settings.py
+++ confessions/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'confessions_api',
+    'corsheaders'
     # 'accounts'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,5 +139,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000', 'http://localhost:8000'
+]
 
 django_heroku.settings(locals())
